0a4fffa7acf1c580bee0546bba47d290a29700be
Dawid Dahl <dawiddahl@gmail.com>
2021-07-28 13:41:31 +0200

    added vuex support

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,40 @@
 <template>
   <div>
     <h1>Hello world!</h1>
-    <h2>COUNTER: {{ this.$store.counter }}</h2>
-    <button @click="myfn">My Button</button>
+    <h2>TODOS:</h2>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">{{ todo.content }}</li>
+    </ul>
+    <p>{{ inputText }}</p>
+    <input v-model="inputText" type="text" :placeholder="inputText" />
+    <button @click="add(inputText)">My Button</button>
   </div>
 </template>
 
 <script>
-import { mapMutations } from 'vuex'
+import { mapMutations, mapState } from 'vuex'
 
 export default {
+  data: () => {
+    return {
+      inputText: ''
+    }
+  },
+  computed: {
+    ...mapState('todos', ['todos'])
+  },
   methods: {
-    myfn: opoop => console.log('Clicked!')
+    ...mapMutations({
+      add: 'todos/ADD_TODO',
+      remove: 'todos/REMOVE_TODO',
+      toggle: 'todos/TOGGLE_TODO'
+    })
   }
 }
 </script>
+
+<style lang="scss" scoped>
+ul {
+  text-decoration: none;
+}
+</style>
