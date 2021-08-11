4c3172becdfb95db2d37cbb1ce0f9c23a3016d97
Dawid Dahl <dawiddahl@gmail.com>
2021-08-11 22:27:48 +0200

    debugging heroku env var issue

--- pages/product/_id.vue
+++ pages/product/_id.vue
@@ -31,7 +31,6 @@ export default {
 		}
 	},
 	mounted() {
-		console.log("LOOOL", process.env.BASE_URL_PROD)
 		console.log("LOOOL1", process.env.ORIGIN_HEADER)
 		console.log("LOOOL2", process.env.NODE_ENV)
 		console.log("LOOOL3", process.env.HOST)
